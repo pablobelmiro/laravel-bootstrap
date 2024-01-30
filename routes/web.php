b2f02d4b630521868761a49467ee22c43b8be7dc
Pablo Belmiro <dev.pablobelmiro@gmail.com>
2024-01-30 12:59:46 -0300

    Implementação Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TarefaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('tarefa', [TarefaController::class, 'show']);
+Route::post('tarefa', [TarefaController::class, 'create']);
+Route::delete('tarefa', [TarefaController::class, 'destroy']);
+Route::delete('tarefa', [TarefaController::class, 'destroy']);
